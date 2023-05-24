24f42d56b5f180457efb40b1b4a38fface3175e1
Margarita Shumilina <margar.maksim@ya.ru>
2023-05-24 20:16:46 +0300

    Версия с другим cors

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
 const apiRouter = require('./routes/api');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(requestLogger);
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: ['https://margarita.nomoredomains.rocks'] }));
+// app.use(cors({ origin: ['https://margarita.nomoredomains.rocks'] }));
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
